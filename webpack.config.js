d561a50b3477742c7e9ba63ec7006720ae61d7cc
David Narvaez <fnarvaez@ubits.co>
2024-06-19 08:58:04 -0500

    Keep calm and commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path')
 const nodeExternals = require('webpack-node-externals')
+const myPackage = require('./package.json')
+const library = myPackage.name
 
 module.exports = {
   entry: './src/index.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    library: 'MiLibreria',
+    library,
     libraryTarget: 'umd',
     globalObject: 'this',
   },
