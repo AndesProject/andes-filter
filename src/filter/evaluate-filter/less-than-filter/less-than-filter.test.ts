467d14f0f03d0850d859988250c281de70bc4df5
David Narvaez <fnarvaez@ubits.co>
2024-08-12 22:02:01 -0500

    LessThanFilter

--- src/filter/evaluate-filter/less-than-filter/less-than-filter.test.ts
+++ src/filter/evaluate-filter/less-than-filter/less-than-filter.test.ts
@@ -1,44 +1,26 @@
 import { describe, expect, it } from 'vitest'
-import { LessThanFilter } from './less-than-filter'
+import { filterFrom } from '../../filter-from'
 
 describe('LessThanFilter', () => {
-  it('debe retornar true cuando el valor es menor que el valor umbral', () => {
-    const filter = new LessThanFilter(10)
-    expect(filter.evaluate(5)).toBe(true)
-  })
+  it('number', () => {
+    const filter = filterFrom<{ size: number }>([
+      { size: -2 },
+      { size: -1 },
+      { size: 0 },
+      { size: 0 },
+      { size: 1 },
+      { size: 2 },
+      { size: 3 },
+    ])
 
-  it('debe retornar false cuando el valor es igual al valor umbral', () => {
-    const filter = new LessThanFilter(10)
-    expect(filter.evaluate(10)).toBe(false)
-  })
+    expect(filter.findMany({ where: { size: { lt: -2 } } }).length).toBe(0)
+    expect(filter.findMany({ where: { size: { lt: -1 } } }).length).toBe(1)
+    expect(filter.findMany({ where: { size: { lt: 0 } } }).length).toBe(2)
+    expect(filter.findMany({ where: { size: { lt: 3 } } }).length).toBe(6)
+    expect(filter.findMany({ where: { size: { lt: 100 } } }).length).toBe(7)
 
-  it('debe retornar false cuando el valor es mayor que el valor umbral', () => {
-    const filter = new LessThanFilter(10)
-    expect(filter.evaluate(15)).toBe(false)
-  })
-
-  it('debe manejar comparaciones de tipo string correctamente', () => {
-    const filter = new LessThanFilter('mango')
-    expect(filter.evaluate('apple')).toBe(true) // 'apple' < 'mango' en orden lexicográfico
-    expect(filter.evaluate('orange')).toBe(false) // 'orange' > 'mango'
-  })
-
-  it('debe manejar comparaciones de tipo boolean correctamente', () => {
-    const filter = new LessThanFilter(true)
-    expect(filter.evaluate(false)).toBe(true)
-    expect(filter.evaluate(true)).toBe(false)
-  })
-
-  it('debe manejar comparaciones de tipo Date correctamente', () => {
-    const earlierDate = new Date('2024-01-01')
-    const laterDate = new Date('2024-12-31')
-    const filter = new LessThanFilter(laterDate)
-    expect(filter.evaluate(earlierDate)).toBe(true)
-    expect(filter.evaluate(laterDate)).toBe(false)
-  })
-
-  it('debe retornar false cuando el tipo del valor no es comparable', () => {
-    const filter = new LessThanFilter(10)
-    expect(filter.evaluate({})).toBe(false) // Comparando un objeto con un número no es válido
+    expect(filter.findUnique({ where: { size: { lt: -2 } } })?.size).toBe(undefined)
+    expect(filter.findUnique({ where: { size: { lt: -1 } } })?.size).toBe(-2)
+    expect(filter.findUnique({ where: { size: { lt: 100 } } })?.size).toBe(-2)
   })
 })
