97e237dd1e73676cb1613643ef884afc87409925
David Narvaez <fnarvaez@ubits.co>
2024-08-21 08:27:13 -0500

    vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     include: ['src/**/*.test.{ts,tsx,js,jsx}'],
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'html'],
+      reporter: ['text', 'json', 'html', 'lcov'],
       reportsDirectory: './coverage',
       exclude: ['node_modules/', 'dist/', '**/*.d.ts', '**/index.ts', '*.cjs'],
     },
