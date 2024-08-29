f5f2c3735912f20d878e4663009732938bf14eb5
David Narvaez <fnarvaez@ubits.co>
2024-08-29 10:01:32 -0500

    test string-contains-filter

--- src/filter/evaluate-filter/string-contains-filter/string-contains-filter.ts
+++ src/filter/evaluate-filter/string-contains-filter/string-contains-filter.ts
@@ -3,7 +3,7 @@ import { EvaluateFilter } from '../evaluate-filter.interface'
 export class StringContainsFilter implements EvaluateFilter {
   constructor(private substring: string) {}
 
-  evaluate(value: any): boolean {
-    return typeof value === 'string' && value.includes(this.substring)
+  evaluate(value: string): boolean {
+    return typeof value === 'string' && Boolean(value.length) && value.includes(this.substring)
   }
 }
