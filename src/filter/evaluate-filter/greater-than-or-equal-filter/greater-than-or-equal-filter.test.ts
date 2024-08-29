6a6a422efcb8c985609bdd4418cc051f6ca144e4
David Narvaez <fnarvaez@ubits.co>
2024-08-29 09:05:26 -0500

    test greater-than-or-equal-filter

--- src/filter/evaluate-filter/greater-than-or-equal-filter/greater-than-or-equal-filter.test.ts
+++ src/filter/evaluate-filter/greater-than-or-equal-filter/greater-than-or-equal-filter.test.ts
@@ -1,52 +1,27 @@
 import { describe, expect, it } from 'vitest'
-import { GreaterThanOrEqualFilter } from './greater-than-or-equal-filter'
+import { filterFrom } from '../../filter-from'
 
 describe('GreaterThanOrEqualFilter', () => {
-  it('debe retornar true cuando el valor es mayor que el valor umbral', () => {
-    const filter = new GreaterThanOrEqualFilter(10)
-    expect(filter.evaluate(15)).toBe(true)
-  })
+  it('number', () => {
+    const filter = filterFrom<{ size: number }>([
+      { size: -2 },
+      { size: -1 },
+      { size: 0 },
+      { size: 0 },
+      { size: 1 },
+      { size: 2 },
+      { size: 3 },
+    ])
 
-  it('debe retornar true cuando el valor es igual al valor umbral', () => {
-    const filter = new GreaterThanOrEqualFilter(10)
-    expect(filter.evaluate(10)).toBe(true)
-  })
+    expect(filter.findMany({ where: { size: { gte: -2 } } }).length).toBe(7)
+    expect(filter.findMany({ where: { size: { gte: -1 } } }).length).toBe(6)
+    expect(filter.findMany({ where: { size: { gte: 0 } } }).length).toBe(5)
+    expect(filter.findMany({ where: { size: { gte: 3 } } }).length).toBe(1)
+    expect(filter.findMany({ where: { size: { gte: 100 } } }).length).toBe(0)
 
-  it('debe retornar false cuando el valor es menor que el valor umbral', () => {
-    const filter = new GreaterThanOrEqualFilter(10)
-    expect(filter.evaluate(5)).toBe(false)
-  })
-
-  it('debe manejar comparaciones de tipo string correctamente', () => {
-    const filter = new GreaterThanOrEqualFilter('mango')
-    expect(filter.evaluate('orange')).toBe(true) // 'orange' > 'mango' en orden lexicográfico
-    expect(filter.evaluate('mango')).toBe(true) // 'mango' == 'mango'
-    expect(filter.evaluate('apple')).toBe(false) // 'apple' < 'mango'
-  })
-
-  it('debe manejar comparaciones de tipo boolean correctamente', () => {
-    const filter = new GreaterThanOrEqualFilter(false)
-    expect(filter.evaluate(true)).toBe(true)
-    expect(filter.evaluate(false)).toBe(true)
-  })
-
-  it('debe manejar comparaciones de tipo Date correctamente', () => {
-    const earlierDate = new Date('2024-01-01')
-    const laterDate = new Date('2024-12-31')
-    const filter = new GreaterThanOrEqualFilter(earlierDate)
-    expect(filter.evaluate(laterDate)).toBe(true)
-    expect(filter.evaluate(earlierDate)).toBe(true)
-    expect(filter.evaluate(new Date('2023-01-01'))).toBe(false)
-  })
-
-  it('debe retornar false cuando el tipo del valor no es comparable', () => {
-    const filter = new GreaterThanOrEqualFilter(10)
-    expect(filter.evaluate({})).toBe(false) // Comparando un objeto con un número no es válido
-  })
-
-  it('debe manejar valores nulos y undefined', () => {
-    const filter = new GreaterThanOrEqualFilter(10)
-    expect(filter.evaluate(null)).toBe(false)
-    expect(filter.evaluate(undefined)).toBe(false)
+    expect(filter.findUnique({ where: { size: { gte: -3 } } })?.size).toBe(-2)
+    expect(filter.findUnique({ where: { size: { gte: -2 } } })?.size).toBe(-2)
+    expect(filter.findUnique({ where: { size: { gte: -1 } } })?.size).toBe(-1)
+    expect(filter.findUnique({ where: { size: { gte: 100 } } })?.size).toBe(undefined)
   })
 })
