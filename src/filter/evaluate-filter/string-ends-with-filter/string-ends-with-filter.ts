844fcbb2a55c140df7d12056e563d187d4e0b05a
David Narvaez <fnarvaez@ubits.co>
2024-08-29 10:12:42 -0500

    test string-ends-with-filter

--- src/filter/evaluate-filter/string-ends-with-filter/string-ends-with-filter.ts
+++ src/filter/evaluate-filter/string-ends-with-filter/string-ends-with-filter.ts
@@ -3,7 +3,7 @@ import { EvaluateFilter } from '../evaluate-filter.interface'
 export class StringEndsWithFilter implements EvaluateFilter {
   constructor(private suffix: string) {}
 
-  evaluate(value: any): boolean {
+  evaluate(value: string): boolean {
     return typeof value === 'string' && value.endsWith(this.suffix)
   }
 }
